ffe6ec131557b64bd021361be90db44642b74810
Ryan Achten <ryanachten@gmail.com>
2017-09-08 10:00:42 +1200

    Add home page template

---
+++ mysite/code/HomePage.php
@@ -0,0 +1,13 @@
+<?php
+  class HomePage extends Page{
+
+  }
+
+  class HomePage_Controller extends Page_Controller{
+
+    public function init(){
+      parent::init();
+      Requirements::css("{$this->ThemeDir()}/css/home.css");
+    }
+  }
+ ?>
